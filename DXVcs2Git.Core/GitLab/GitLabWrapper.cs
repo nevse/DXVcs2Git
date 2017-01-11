673903e58d3c4f16d99c971115566188c5e39763
Maxim Litvinov <maximatorr@bk.ru>
2017-01-11 20:09:38 +0300

    move git gitlab.net api

--- DXVcs2Git.Core/GitLab/GitLabWrapper.cs
+++ DXVcs2Git.Core/GitLab/GitLabWrapper.cs
@@ -2,163 +2,118 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using DXVcs2Git.Core;
-using NGitLab;
-using NGitLab.Models;
-using User = NGitLab.Models.User;
+using GitLab.NET;
+using GitLab.NET.ResponseModels;
+using User = GitLab.NET.ResponseModels.User;
 
 namespace DXVcs2Git.Git {
     public class GitLabWrapper {
         const string IgnoreValidation = "[IGNOREVALIDATION]";
         readonly GitLabClient client;
         public GitLabWrapper(string server, string token) {
-            client = GitLabClient.Connect(server, token);
+            client = new GitLabClient(new Uri(server), token);
         }
-        public bool IsAdmin() {
-            return client.Users.Current.IsAdmin;
+        public async Task<bool> IsAdmin() {
+            var currentUser = await client.Users.GetCurrent();
+            return currentUser.Data.IsAdmin ?? false;
         }
-        public IEnumerable<Project> GetProjects() {
-            return client.Projects.Accessible;
+        public async Task<IEnumerable<Project>> GetProjects() {
+            return (await client.Projects.Accessible()).Data;
         }
-        public IEnumerable<Project> GetAllProjects() {
-            return client.Projects.All;
+        public async Task<IEnumerable<Project>> GetAllProjects() {
+            return (await client.Projects.GetAll()).Data;
         }
-        public Project GetProject(int id) {
-            return this.client.Projects[id];
+        public async Task<Project> GetProject(uint id) {
+            return (await this.client.Projects.Find(id)).Data;
         }
-        public Project FindProject(string project) {
-            return client.Projects.Accessible.FirstOrDefault(x => 
-                string.Compare(x.HttpUrl, project, StringComparison.InvariantCultureIgnoreCase) == 0 ||
+        public async Task<Project> FindProject(string project) {
+            return (await client.Projects.Accessible()).Data.FirstOrDefault(x =>
+                string.Compare(x.HttpUrlToRepo, project, StringComparison.InvariantCultureIgnoreCase) == 0 ||
                 string.Compare(x.SshUrl, project, StringComparison.InvariantCultureIgnoreCase) == 0);
         }
-        public Project FindProjectFromAll(string project) {
-            return client.Projects.All.FirstOrDefault(x => 
-                string.Compare(x.HttpUrl, project, StringComparison.InvariantCultureIgnoreCase) == 0 ||
+        public async Task<Project> FindProjectFromAll(string project) {
+            return (await GetAllProjects()).FirstOrDefault(x =>
+                string.Compare(x.HttpUrlToRepo, project, StringComparison.InvariantCultureIgnoreCase) == 0 ||
                 string.Compare(x.SshUrl, project, StringComparison.InvariantCultureIgnoreCase) == 0);
         }
-        public IEnumerable<MergeRequest> GetMergeRequests(Project project, Func<MergeRequest, bool> mergeRequestsHandler = null) {
+        public async Task<IEnumerable<MergeRequest>> GetMergeRequests(Project project, Func<MergeRequest, bool> mergeRequestsHandler = null) {
             mergeRequestsHandler = mergeRequestsHandler ?? (x => true);
-            var mergeRequestsClient = client.GetMergeRequest(project.Id);
-            return mergeRequestsClient.AllInState(MergeRequestState.opened).Where(x => mergeRequestsHandler(x));
+            var mergeRequests = await client.MergeRequests.GetAll(project.Id, MergeRequestState.Opened);
+            return mergeRequests.Data.Where(x => mergeRequestsHandler(x));
         }
-        public MergeRequest GetMergeRequest(Project project, int id) {
-            var mergeRequestsClient = client.GetMergeRequest(project.Id);
-            return mergeRequestsClient[id];
+        public async Task<MergeRequest> GetMergeRequest(Project project, uint id) {
+            return await GetMergeRequest(project.Id, id);
         }
-        public IEnumerable<MergeRequestFileData> GetMergeRequestChanges(MergeRequest mergeRequest) {
-            var mergeRequestsClient = client.GetMergeRequest(mergeRequest.ProjectId);
-            var changesClient = mergeRequestsClient.Changes(mergeRequest.Id);
-            return changesClient.Changes.Files;
+        public async Task<MergeRequest> GetMergeRequest(uint projectId, uint id) {
+            var mergeRequest = await client.MergeRequests.Find(projectId, id);
+            return mergeRequest.Data;
         }
-        public IEnumerable<Commit> GetMergeRequestCommits(MergeRequest mergeRequest) {
-            var mergeRequestsClient = client.GetMergeRequest(mergeRequest.ProjectId);
-            return mergeRequestsClient.Commits(mergeRequest.Id).All;
+        public IEnumerable<CommitDiff> GetMergeRequestChanges(MergeRequest mergeRequest) {
+            return mergeRequest.Changes;
         }
-        public MergeRequest ProcessMergeRequest(MergeRequest mergeRequest, string comment) {
-            var mergeRequestsClient = client.GetMergeRequest(mergeRequest.ProjectId);
-            try {
-                return mergeRequestsClient.Accept(mergeRequest.Id, new MergeCommitMessage() { Message = comment });
-            }
-            catch (Exception ex) {
-                Log.Error("Merging has thrown exception", ex);
-                mergeRequest.State = "merging_failed";
-                return mergeRequest;
-            }
+        public async Task<IEnumerable<Commit>> GetMergeRequestCommits(MergeRequest mergeRequest) {
+            var commits = await client.MergeRequests.GetCommits(mergeRequest.ProjectId.Value, mergeRequest.Id.Value);
+            return commits.Data;
         }
-        public MergeRequest UpdateMergeRequestTitleAndDescription(MergeRequest mergeRequest, string title, string description) {
-            var mergeRequestsClient = client.GetMergeRequest(mergeRequest.ProjectId);
-            try {
-                return mergeRequestsClient.Update(mergeRequest.Id, new MergeRequestUpdate() {
-                    Description = description,
-                    Title = title,
-                    AssigneeId = mergeRequest.Assignee?.Id,
-                    SourceBranch = mergeRequest.SourceBranch,
-                    TargetBranch = mergeRequest.TargetBranch,
-                });
-            }
-            catch {
-                return mergeRequestsClient[mergeRequest.Id];
-            }
-        }
-        public MergeRequest CloseMergeRequest(MergeRequest mergeRequest) {
-            var mergeRequestsClient = client.GetMergeRequest(mergeRequest.ProjectId);
-            try {
-                return mergeRequestsClient.Update(mergeRequest.Id, new MergeRequestUpdate() {
-                    NewState = "close",
-                    AssigneeId = mergeRequest.Assignee?.Id,
-                    SourceBranch = mergeRequest.SourceBranch,
-                    TargetBranch = mergeRequest.TargetBranch,
-                    Title = mergeRequest.Title,
-                    Description = mergeRequest.Description,
-                });
-            }
-            catch {
-                return mergeRequestsClient[mergeRequest.Id];
-            }
-        }
-        public MergeRequest CreateMergeRequest(Project origin, Project upstream, string title, string description, string user, string sourceBranch, string targetBranch) {
-            var mergeRequestClient = this.client.GetMergeRequest(origin.Id);
-            var mergeRequest = mergeRequestClient.Create(new MergeRequestCreate() {
-                Title = title,
-                Description = description,
-                SourceBranch = sourceBranch,
-                TargetBranch = targetBranch,
-                TargetProjectId = upstream.Id,
-            });
+        public async Task<MergeRequest> ProcessMergeRequest(MergeRequest mergeRequest, string comment) {
+            await client.MergeRequests.CreateComment(mergeRequest.ProjectId.Value, mergeRequest.Id.Value, comment);
             return mergeRequest;
         }
-        public MergeRequest ReopenMergeRequest(MergeRequest mergeRequest, string autoMergeFailedComment) {
-            var mergeRequestsClient = client.GetMergeRequest(mergeRequest.ProjectId);
-            try {
-                return mergeRequestsClient.Update(mergeRequest.Id, new MergeRequestUpdate() {NewState = "reopen", Description = autoMergeFailedComment});
-            }
-            catch {
-                return mergeRequestsClient[mergeRequest.Id];
-            }
+        public async Task<MergeRequest> UpdateMergeRequestTitleAndDescription(MergeRequest mergeRequest, string title, string description) {
+            await client.MergeRequests.Update(mergeRequest.ProjectId.Value, mergeRequest.Id.Value, mergeRequest.TargetBranch, title, description, mergeRequest.Assignee.Id);
+            return await GetMergeRequest(mergeRequest.ProjectId.Value, mergeRequest.Id.Value);
         }
-        public User GetUser(int id) {
-            return this.client.Users[id];
+        public async Task<MergeRequest> CloseMergeRequest(MergeRequest mergeRequest) {
+            await client.MergeRequests.Update(mergeRequest.ProjectId.Value, mergeRequest.Id.Value, mergeRequest.TargetBranch, state: MergeRequestStateEvent.Close);
+            return await GetMergeRequest(mergeRequest.ProjectId.Value, mergeRequest.Id.Value);
         }
-        public IEnumerable<User> GetUsers() {
+        public async Task<MergeRequest> CreateMergeRequest(Project origin, Project upstream, string title, string description, string user, string sourceBranch, string targetBranch) {
+            var mergeRequest = await client.MergeRequests.Create(origin.Id, sourceBranch, targetBranch, title, description, targetProjectId: upstream.Id);
+            return mergeRequest.Data;
+        }
+        public async Task<User> GetUser(uint id) {
+            return (await client.Users.Find(id)).Data;
+        }
+        public async Task<IEnumerable<User>> GetUsers() {
             var usersClient = this.client.Users;
-            return usersClient.All.ToList();
+            return (await usersClient.GetAll()).Data;
         }
-        public void RegisterUser(string userName, string displayName, string email) {
-            try {
-                var userClient = this.client.Users;
-                var userUpsert = new UserUpsert() { Username = userName, Name = displayName, Email = email, Password = new Guid().ToString() };
-                userClient.Create(userUpsert);
-            }
-            catch (Exception ex) {
-                Log.Error($"Can`t create user {userName} email {email}", ex);
-                throw;
-            }
+        public async Task<User> RegisterUser(string userName, string displayName, string email) {
+            var user = await client.Users.Create(email, new Guid().ToString(), userName, displayName, projectsLimit: 10, confirm: true);
+            return user.Data;
+            //try {
+
+            //    var userClient = this.client.Users;
+            //    var userUpsert = new UserUpsert() { Username = userName, Name = displayName, Email = email, Password = new Guid().ToString() };
+            //    userClient.Create(userUpsert);
+            //}
+            //catch (Exception ex) {
+            //    Log.Error($"Can`t create user {userName} email {email}", ex);
+            //    throw;
+            //}
         }
-        public User RenameUser(User gitLabUser, string userName, string displayName, string email) {
-            try {
-                var userClient = this.client.Users;
-                var userUpsert = new UserUpsert() { Username = userName, Name = displayName, Email = email, Password = new Guid().ToString() };
-                return userClient.Update(gitLabUser.Id, userUpsert);
-            }
-            catch (Exception ex) {
-                Log.Error($"Can`t change user {userName} email {email}", ex);
-                throw;
-            }
+        public async Task<User> RenameUser(User gitLabUser, string userName, string displayName, string email) {
+            var user = await client.Users.Update((uint)gitLabUser.Id, email: email, username: userName, name: displayName);
+            return user.Data;
+            //try {
+            //    var userClient = this.client.Users;
+            //    var userUpsert = new UserUpsert() { Username = userName, Name = displayName, Email = email, Password = new Guid().ToString() };
+            //    return userClient.Update(gitLabUser.Id, userUpsert);
+            //}
+            //catch (Exception ex) {
+            //    Log.Error($"Can`t change user {userName} email {email}", ex);
+            //    throw;
+            //}
         }
-        public Branch GetBranch(Project project, string branch) {
-            var repo = this.client.GetRepository(project.Id);
-            var branchesClient = repo.Branches;
-            try {
-                return branchesClient[branch];
-            }
-            catch {
-                return branchesClient.All.FirstOrDefault(x => x.Name == branch);
-            }
+        public async Task<Branch> GetBranch(Project project, string branchName) {
+            var branch = await client.Branches.Find(project.Id, branchName);
+            return branch.Data;
         }
-        public IEnumerable<Branch> GetBranches(Project project) {
-            var repo = this.client.GetRepository(project.Id);
-            var branchesClient = repo.Branches;
-            return branchesClient.All;
+        public async Task<IEnumerable<Branch>> GetBranches(Project project) {
+            var branches = await client.Branches.GetAll(project.Id);
+            return branches.Data;
         }
         public MergeRequest UpdateMergeRequestAssignee(MergeRequest mergeRequest, string user) {
             var userInfo = GetUsers().FirstOrDefault(x => x.Username == user);
@@ -196,11 +151,11 @@ namespace DXVcs2Git.Git {
         public ProjectHook CreateProjectHook(Project project, Uri url, bool mergeRequestEvents, bool pushEvents, bool buildEvents) {
             var projectClient = client.GetRepository(project.Id);
             var projectHooks = projectClient.ProjectHooks;
-            return projectHooks.Create(new ProjectHookUpsert() { MergeRequestsEvents = mergeRequestEvents, PushEvents = pushEvents, BuildEvents = buildEvents, Url = url, EnableSSLVerification = false});
+            return projectHooks.Create(new ProjectHookUpsert() { MergeRequestsEvents = mergeRequestEvents, PushEvents = pushEvents, BuildEvents = buildEvents, Url = url, EnableSSLVerification = false });
         }
         public ProjectHook UpdateProjectHook(Project project, ProjectHook hook, Uri uri, bool mergeRequestEvents, bool pushEvents, bool buildEvents) {
             var repository = this.client.GetRepository(project.Id);
-            return repository.ProjectHooks.Update(hook.Id, new ProjectHookUpsert() { Url = uri, MergeRequestsEvents = mergeRequestEvents, PushEvents = pushEvents, BuildEvents = buildEvents, EnableSSLVerification = false});
+            return repository.ProjectHooks.Update(hook.Id, new ProjectHookUpsert() { Url = uri, MergeRequestsEvents = mergeRequestEvents, PushEvents = pushEvents, BuildEvents = buildEvents, EnableSSLVerification = false });
         }
         public IEnumerable<Comment> GetComments(MergeRequest mergeRequest) {
             var mergeRequestsClient = client.GetMergeRequest(mergeRequest.ProjectId);
